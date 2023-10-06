9f995f013fbd7d6e1a63eaaa96deacd3ac2a2ec0
M-Saqib-Naeem <saqib.naeem123@gmail.com>
2023-10-06 19:08:42 +0500

    first commit

---
+++ web/views/products/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\Products $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="products-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'product_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'product_price')->textInput() ?>
+
+    <?= $form->field($model, 'product_sku')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
