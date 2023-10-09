a0745980f432d79d12cedf4f0146dea1281caac4
M-Saqib-Naeem <saqib.naeem123@gmail.com>
2023-10-09 13:57:35 +0500

    Learning different type of inline Controller actions also enabled the Pretty URLs

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -2,11 +2,35 @@
 
 namespace app\controllers;
 
-class ProductController extends \yii\web\Controller
+use yii\web\Controller;
+
+// Route: product > Product > ProductController > app\controllers\ProductController
+class ProductController extends Controller
 {
-    public function actionIndex()
-    {
+    /**
+     * Change the default action to custom defualt.
+     * By default its set to index method
+     */
+    public $defaultAction = "route-name";
+
+
+    // Route: product/index > Product/Index > ProductController/ActionIndex
+    public function actionIndex() {
+        $title = "saa";
         return $this->render('index');
     }
 
+    // Route: product/products > Product/Products > ProductController/ActionProducts
+    public function actionProducts() {
+        echo "Hello WOrld";
+    }
+
+    // Route: product/products-details > Product/Product-Details > ProductController/ProductDetails > ProductController/ActionProductDetails
+    public function actionProductDetails() {
+        echo "This is product details";
+    }
+
+    public function actionProductDetailsWithId( $first = "Saqib", $last = "Naeem" ) {
+        echo "This is product details $first $last";
+    }
 }
--- views/product/index.php
+++ views/product/index.php
@@ -1,9 +1,11 @@
 <?php
 /** @var yii\web\View $this */
+$this->registerLinkTag(['description' => 'everything']);
+$this->registerLinkTag(['title' => 'everything']);
 ?>
+
 <h1>product/index</h1>
 
 <p>
     You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
 </p>
